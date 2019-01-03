90b5f01288e7b684cc3631929e4f659c5b496072
seema0789 <45092329+seema0789@users.noreply.github.com>
2019-01-03 19:58:17 +0530

    Update index.php

--- website/index.php
+++ website/index.php
@@ -7,7 +7,7 @@
 	    <h2>Candidate Information Form</h2>
 	    <ul>
 		<?php
-  		    $json = file_get_contents('http://product-service');
+  		    $json = file_get_contents('10.100.130.23:80');
   		    $obj = json_decode($json);
 		    $products = $obj->products;
 		    foreach ($products as $product) {
